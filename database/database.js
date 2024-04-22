97cef9d94bf8264a2e7f1c32b087893a3a739230
Fabricio Oliveira <fabricio.oliveira.s@hotmail.com>
2024-04-21 22:56:18 -0300

    DB Access

--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
+const dbConfig = require('./connection');
 
-const connection = new Sequelize('Study','root','admin123',{
+const connection = new Sequelize('Study',dbConfig.user,dbConfig.password,{
     root:'localhost',
     dialect:'mysql'
 });
